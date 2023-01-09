79d5a772517b7eab8c103c205e201967c187dc44
Lukas Frey <mail@lukasfrey.cz>
2023-01-09 19:37:58 +0100

    chore: package setup

---
+++ src/PopulatorServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Guava\LaravelPopulator;
+
+use Illuminate\Support\ServiceProvider;
+
+class PopulatorServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+
+}
