4b420fb6c01aa6529927e22c7c8140f4287a135a
Lukas Frey <mail@lukasfrey.cz>
2023-01-18 20:44:43 +0100

    refactor: improved code, added exceptions
    Signed-off-by: Lukas Frey <mail@lukasfrey.cz>

--- src/Population/Processor.php
+++ src/Population/Processor.php
@@ -3,6 +3,7 @@
 namespace Guava\LaravelPopulator\Population;
 
 use Guava\LaravelPopulator\Exceptions\InvalidSampleException;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
@@ -10,6 +11,7 @@ use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 /**
@@ -55,49 +57,35 @@ class Processor
      */
     protected function relations(Collection $data): Collection
     {
-//        if (!$data->has('relations')) {
-//            return $data;
-//        }
-//
-//        if (empty($data->get('relations', []))) {
-//            return $data;
-//        }
-
         return $data
-            ->pipe(fn($collection) => $collection
-//                ->except(['relations'])
-//                ->mergeRecursive(
-//                    collect($data)
-                        ->mapWithKeys(function ($value, $relationName) use ($data) {
-                            if ($this->sample->model->isRelation($relationName)) {
-                                $relation = $this->sample->model->$relationName();
+//            ->pipe(fn($collection) => $collection
+                ->mapWithKeys(function ($value, $relationName) {
+                    if ($this->sample->model->isRelation($relationName)) {
+                        $relation = $this->sample->model->$relationName();
 
-                                if ($relation instanceof MorphTo) {
-                                    return $this->morphTo($relation, $value);
-                                }
+                        if ($relation instanceof MorphTo) {
+                            return $this->morphTo($relation, $value);
+                        }
 
-                                if ($relation instanceof MorphMany) {
-                                    $this->morphMany($relation, $value);
-                                    return [];
-                                }
+                        if ($relation instanceof MorphMany) {
+                            $this->morphMany($relation, $value);
+                            return [];
+                        }
 
-                                if ($relation instanceof BelongsTo) {
-                                    return $this->belongsTo($relation, $value);
-                                }
+                        if ($relation instanceof BelongsTo) {
+                            return $this->belongsTo($relation, $value);
+                        }
 
-                                if ($relation instanceof BelongsToMany) {
-                                    $this->belongsToMany($relation, $value);
-                                    return [];
-                                }
-                            } else {
-                                return [$relationName => $value];
-                            }
+                        if ($relation instanceof BelongsToMany) {
+                            $this->belongsToMany($relation, $value);
+                            return [];
+                        }
+                    } else {
+                        return [$relationName => $value];
+                    }
 
-                            throw new InvalidSampleException('Relations are misconfigured in ' . $this->file->getFilename());
-                        })
-                )
-//            ->when(fn(Collection $collection) => $this->sample->model::class === 'App\Models\Post',
-//            fn(Collection $collection) => $collection->dd())
+                    throw new InvalidSampleException('Relations are misconfigured in ' . $this->file->getFilename());
+                })
             ;
     }
 
@@ -107,21 +95,15 @@ class Processor
      * @param BelongsTo $relation
      * @param string $value
      * @return array
+     * @throws InvalidSampleException
      */
     protected function belongsTo(BelongsTo $relation, string $value): array
     {
-        // Load from memory
-        $id = $this->sample->populator->memory->get($relation->getRelated()::class, $value);
+        $id = $this->getPrimaryIdFromMemory($relation->getRelated(), $value);
 
         if (!$id) {
-            // Load by ID
-            if (is_numeric($value)) {
-                $id = $value;
-                // Load
-            } else {
-                [$key, $val] = explode(':', $value, 2);
-                $id = DB::table($relation->getRelated()->getTable())->where($key, $val)->first()->id;
-            }
+            $sampleName = $this->sample->model::class;
+            throw new InvalidSampleException("Item {$this->name} from Sample {$sampleName} has an invalid belongsTo relation set for {$relation->getRelationName()} (value: {$value}).");
         }
 
         return [$relation->getForeignKeyName() => $id];
@@ -137,7 +119,13 @@ class Processor
     protected function belongsToMany(BelongsToMany $relation, array $value): void
     {
         foreach ($value as $identifier) {
-//            dump($relation->getRelated()::class, $identifier, $this->sample->populator->memory->get($relation->getRelated()::class, $identifier));
+            $id = $this->getPrimaryIdFromMemory($relation->getRelated(), $identifier);
+
+            if (!$id) {
+                $sampleName = $this->sample->model::class;
+                throw new InvalidSampleException("Item {$this->name} from Sample {$sampleName} has an invalid belongsToMany relation set for {$relation->getRelationName()} (value: {$identifier}).");
+            }
+
             $this->memory->set($relation->getTable(), $identifier, [
                 'relation' => 'belongsToMany',
                 'foreign' => [
@@ -145,7 +133,7 @@ class Processor
                 ],
                 'related' => [
                     'pivot_key' => $relation->getRelatedPivotKeyName(),
-                    'id' => $this->sample->populator->memory->get($relation->getRelated()::class, $identifier),
+                    'id' => $id,
                 ],
             ]);
         }
@@ -160,7 +148,13 @@ class Processor
      */
     protected function morphTo(MorphTo $relation, array $value): array
     {
-        $id = $this->sample->populator->memory->get($value[1], $value[0]);
+//        $id = $this->sample->populator->memory->get($value[1], $value[0]);
+        $id = $this->getPrimaryIdFromMemory(new $value[1], $value[0]);
+
+        if (!$id) {
+            $sampleName = $this->sample->model::class;
+            throw new InvalidSampleException("Item {$this->name} from Sample {$sampleName} has an invalid belongsToMany relation set for {$relation->getRelationName()} (value: {$identifier}).");
+        }
 
         return [$relation->getForeignKeyName() => $id, $relation->getMorphType() => $value[1]];
     }
@@ -170,59 +164,27 @@ class Processor
      * Processes the belongs to relationship and sets the foreign key.
      *
      * @param MorphMany $relation
-     * @param string $value
-     * @return array
+     * @param array $items
+     * @return void
      */
     protected function morphMany(MorphMany $relation, array $items): void
     {
-//        $relation->getRelated()->getTable()
-//        dd($relation);
-
-//        dd($relation, $items);
-
-
-//        if (is_int($value)) {
-//            $value = Collection::times($value, fn() => []);
-//        }
 
         $index = 0;
         foreach ($items as $item) {
-//            $processor = new Processor($sample);
-
             $morphName = Str::beforeLast($relation->getForeignKeyName(), '_');
             $item = collect($item)->merge([
                 $morphName => [$this->name, $relation->getMorphClass()],
             ])->toArray();
 
             $otherMorphName = Str::before($relation->getQualifiedForeignKeyName(), '.');
-//            dump($relation->getRelated()->getTable(), "{$this->name}_{$otherMorphName}_{$index}");
 
             $this->memory->set($relation->getRelated()->getTable(), "{$this->name}_{$otherMorphName}_{$index}", [
                 'relation' => 'morphMany',
                 'related' => $relation->getRelated()::class,
                 'model' => $item,
-//                'morph' => [
-//                    'related' => $relation->getRelated()::class,
-//                    'name' => $morphName,
-//                    'foreign' => $relation->getForeignKeyName(),
-//                    'type' => $relation->getMorphType(),
-//                    'class' => $relation->getMorphClass(),
-//                ],
-//                'data' => $unit,
             ]);
             $index++;
-
-//                'foreign' => $relation->getForeignKeyName(),
-//                'type' => $relation->getMorphType(),
-//                'class' => $relation->getMorphClass(),
-//                'related' => $unit,
-// TODO: Reuse code from BelongsTo
-//                'data' => collect($unit)->mapWithKeys(function($identifier, $relation) {
-//                    $id = $this->sample->populator->memory->get($relation->getRelated()::class, $identifier);
-//
-//                    return [$relation->getForeignKeyName() => $id];
-//                }),
-//            ]);
         }
     }
 
@@ -280,9 +242,7 @@ class Processor
         foreach ($this->memory->all() as $table => $relations) {
 
             foreach ($relations as $name => $relation) {
-//                if (!Arr::has($relation,'relation')) {
-//                    dd($relations);
-//                }
+
                 if ($relation['relation'] === 'belongsToMany') {
                     DB::table($table)
                         ->insert([
@@ -295,28 +255,38 @@ class Processor
                     $sample = Sample::make($relation['related']);
                     $sample->populator = $this->sample->populator;
 
-//                    dd($name);
                     $processor = new Processor($sample);
                     $processor->process($relation['model'], $name);
-
-//                    $morphName = Str::beforeLast($relation->getForeignKeyName(), '_');
-//                    $unit = collect($unit)
-//                        ->mergeRecursive([
-//                        'relations' => [
-//                            $morphName => [$this->name, $relation->getMorphClass()],
-//                        ]
-//                    ])->toArray();
-//                    DB::table($table)
-//                        ->insert([
-//                            Str::after($relation['foreign'], '.') => $id,
-//                            Str::after($relation['type'], '.') => $relation['class'],
-//                        ]);
                 }
             }
 
         }
 
         return $data;
+    }
+
+    protected function getPrimaryIdFromMemory(Model $model, string $identifier): int|string|null {
+        $id = $this->sample->populator->memory->get($model::class, $identifier);
+
+        // Load from memory
+        if ($id) {
+            return $id;
+        }
+
+        // Load from database via primary key
+        if (DB::table($model->getTable())->where($model->getKeyName(), $identifier)->first()) {
+            return $identifier;
+        }
+
+        // Load from database via unique key
+        if (str_contains($identifier, ':')) {
+            [$key, $value] = explode(':', $identifier, 2);
+
+            // TODO: might return null
+            return DB::table($model->getTable())->where($key, $value)->first()->id;
+        }
+
+        return null;
     }
 
     /**
